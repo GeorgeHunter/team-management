893c33a707253337b0ed5f85f3083678d002a2c5
GeorgeHunter <georgehunter025@gmail.com>
2017-03-01 23:17:26 +0000

    more fixes for user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'roll' => 'admin',
+            'role' => 'admin',
             'password' => bcrypt($data['password']),
         ]);
     }
