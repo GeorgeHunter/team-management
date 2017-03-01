141036156eb37816b551442f115f199df0190480
GeorgeHunter <georgehunter025@gmail.com>
2017-03-01 23:15:10 +0000

    fix hardcode of admin role

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,6 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
-            'roll' => 'admin',
             'password' => 'required|min:6|confirmed',
         ]);
     }
@@ -66,6 +65,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'roll' => 'admin',
             'password' => bcrypt($data['password']),
         ]);
     }
