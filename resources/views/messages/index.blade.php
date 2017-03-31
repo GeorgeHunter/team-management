41e6e8f1aa3a0a6e676f8e3b50c003aacd21db86
GeorgeHunter <georgehunter025@gmail.com>
2017-03-31 18:37:37 +0100

    move messages into vue component

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -6,6 +6,8 @@
 
         {{--{{ $messages }}--}}
 
+        <message></message>
+
         <div id="accordion" role="tablist" aria-multiselectable="true">
 
 
@@ -13,35 +15,36 @@
 
                 @if (Auth::User()->player && Auth::User()->player->email == $message->from)
 
-                <div class="card mb-4" style="position: relative;">
-                    @if (!$message->read)
-                        <form method="POST" action="/messages/{{ $message->id }}/mark-as-read" class="d-inline-block" style="position: absolute; right: 6px; top: 6px;">
-                            <input type="number" value="{{$message->id}}" hidden name="message_id">
-                            <button type="submit" style="border: none; background: transparent;"><i class="fa fa-circle-o fa-2x text-warning" aria-hidden="true"></i></button>
-                        </form>
-                    @else
-                        <form method="POST" action="/messages/{{ $message->id }}/mark-as-unread" class="d-inline-block" style="position: absolute; right: 6px; top: 8px;">
-                            <input type="number" value="{{$message->id}}" hidden name="message_id">
-                            <button type="submit" style="border: none; background: transparent;"><i class="fa fa-check-circle-o fa-2x text-success" aria-hidden="true"></i></button>
-                        </form>
+
+                {{--<div class="card mb-4" style="position: relative;">--}}
+                    {{--@if (!$message->read)--}}
+                        {{--<form method="POST" action="/messages/{{ $message->id }}/mark-as-read" class="d-inline-block" style="position: absolute; right: 6px; top: 6px;">--}}
+                            {{--<input type="number" value="{{$message->id}}" hidden name="message_id">--}}
+                            {{--<button type="submit" style="border: none; background: transparent;"><i class="fa fa-circle-o fa-2x text-warning" aria-hidden="true"></i></button>--}}
+                        {{--</form>--}}
+                    {{--@else--}}
+                        {{--<form method="POST" action="/messages/{{ $message->id }}/mark-as-unread" class="d-inline-block" style="position: absolute; right: 6px; top: 8px;">--}}
+                            {{--<input type="number" value="{{$message->id}}" hidden name="message_id">--}}
+                            {{--<button type="submit" style="border: none; background: transparent;"><i class="fa fa-check-circle-o fa-2x text-success" aria-hidden="true"></i></button>--}}
+                        {{--</form>--}}
                         {{--<i class="fa fa-check-circle-o fa-2x text-success mr-2" aria-hidden="true" style="position: absolute; right: 6px; top: 8px;"></i>--}}
-                    @endif
-                    <div class="card-header" role="tab" id="tab{{$message->id}}">
-                        <div data-toggle="collapse" href="#collapse{{$message->id}}" aria-expanded="true" aria-controls="collapse{{$message->id}}">
+                    {{--@endif--}}
+                    {{--<div class="card-header" role="tab" id="tab{{$message->id}}">--}}
+                        {{--<div data-toggle="collapse" href="#collapse{{$message->id}}" aria-expanded="true" aria-controls="collapse{{$message->id}}">--}}
 
-                            {{ $message->from }} | <strong>{{ $message->sent }}</strong>
-                        </div>
-                    </div>
+                            {{--{{ $message->from }} | <strong>{{ $message->sent }}</strong>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
 
-                    <div id="collapse{{$message->id}}" class="collapse @if(!$message->read) show @endif" role="tabpanel" aria-labelledby="headingOne">
-                        <div class="card-block">
-                            <div class="mb-4"><strong>{{ $message->subject }}</strong></div>
-                            <div>{!! $message->body !!}</div>
-                        </div>
-                    </div>
+                    {{--<div id="collapse{{$message->id}}" class="collapse @if(!$message->read) show @endif" role="tabpanel" aria-labelledby="headingOne">--}}
+                        {{--<div class="card-block">--}}
+                            {{--<div class="mb-4"><strong>{{ $message->subject }}</strong></div>--}}
+                            {{--<div>{!! $message->body !!}</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
 
 
-                </div>
+                {{--</div>--}}
 
                     @else
                     @if ($loop->first)
