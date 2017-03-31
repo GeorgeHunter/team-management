a494fd8572c4b6a1477a973c2f85073fa63078ee
GeorgeHunter <georgehunter025@gmail.com>
2017-03-31 18:35:26 +0100

    updates to model and Kernel

--- app/Opponent.php
+++ app/Opponent.php
@@ -6,12 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Opponent extends Model
 {
+
+    protected $fillable = ['name'];
+
     public function venue()
     {
         return $this->hasOne('App\Venue');
     }
+
     public function matches()
     {
         return $this->hasMany('App\Match');
     }
+
 }
