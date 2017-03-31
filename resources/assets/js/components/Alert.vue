6c07ec1805c2082533052c4b6d9e04574d179563
GeorgeHunter <georgehunter025@gmail.com>
2017-03-31 18:32:37 +0100

    add simple vue alert component

---
+++ resources/assets/js/components/Alert.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="alert" :class="alertClass" v-show="showMessage" @click="dismissMessage">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'status'
+        ],
+        data() {
+            return {
+                showMessage: true,
+                alertStatus: this.status,
+                alertClass: ''
+            }
+        },
+        methods: {
+            dismissMessage() {
+                this.showMessage = false;
+            }
+        },
+        created() {
+            let self = this;
+            setTimeout( function () {
+                self.showMessage = false;
+            }, 4000);
+            if (this.alertStatus) {
+                this.alertClass = 'alert-' + this.alertStatus;
+            }
+        }
+
+    }
+</script>
